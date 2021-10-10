175b3ee7287019252d44065ef44729404dc18cee
leesh7048 <tmdgus7048@naver.com>
2021-10-10 16:24:53 +0900

    start

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,8 @@
-import "./app.css";
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import styles from "./app.module.css";
 function App() {
-  return <h1>hello</h1>;
+  return <BrouserRouter></BrouserRouter>;
 }
 
 export default App;
