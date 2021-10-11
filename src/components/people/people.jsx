ad951113308c38ffd6171457071f0b0a07ba2412
leesh7048 <tmdgus7048@naver.com>
2021-10-11 16:00:02 +0900

    초안

---
+++ src/components/people/people.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const People = (props) => {};
+
+export default People;
---
+++ src/components/publications/publications.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Publications = (props) => {};
+
+export default Publications;
