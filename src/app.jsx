19f67a598ba5a90320c18ee0bdfbc15c6da83a9e
leesh7048 <tmdgus7048@naver.com>
2021-10-11 16:22:45 +0900

    router추가

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import styles from "./app.module.css";
 import DefaultLayout from "./components/defaultLayout/defaultLayout";
 import Home from "./components/home/home";
+import Research from "./components/research/research";
+import People from "./components/people/people";
+import Publications from "./components/publications/publications";
+import Activities from "./components/activities/activities";
+
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +15,18 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/research">
+            <Research />
+          </Route>
+          <Route path="/people">
+            <People />
+          </Route>
+          <Route exact path="/publications">
+            <Publications />
+          </Route>
+          <Route exact path="/activities">
+            <Activities />
           </Route>
         </Switch>
       </DefaultLayout>
