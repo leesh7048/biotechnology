cf9f89d63ffbcdcb2662e55625bbe97dfac9b439
leesh7048 <tmdgus7048@naver.com>
2021-10-12 17:32:42 +0900

    .

--- src/components/people/people.jsx
+++ src/components/people/people.jsx
@@ -1,5 +1,40 @@
 import React from "react";
+import styles from "./people.module.css";
+import profile_img from "./img/man.jpg";
 
-const People = (props) => {};
+const People = (props) => {
+  return (
+    <main className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.header_innerBox}>
+          <h1 className={styles.title}>People</h1>
+        </div>
+      </header>
+      <article className={styles.article}>
+        <section className={styles.section}>
+          <div className={styles.profileBox1}>
+            <div className={styles.profile}>
+              <img src={profile_img} alt="man" className={styles.people1} />
+              <h1 className={styles.name}>이택규</h1>
+              <ul>
+                <li>list1</li>
+                <li>list2</li>
+                <li>list3</li>
+              </ul>
+            </div>
+            <div className={styles.comment}>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos ipsum modi, blanditiis fugit pariatur asperiores
+                obcaecati ipsam corrupti veniam vitae vero provident
+                voluptatibus similique corporis est alias labore sed nisi!
+              </span>
+            </div>
+          </div>
+        </section>
+      </article>
+    </main>
+  );
+};
 
 export default People;
--- src/components/research/research.jsx
+++ src/components/research/research.jsx
@@ -1,5 +1,42 @@
 import React from "react";
+import styles from "./research.module.css";
 
-const Research = (props) => {};
+const Research = (props) => {
+  return (
+    <main className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.header_innerBox}>
+          <h1 className={styles.title}>Research</h1>
+        </div>
+      </header>
+      <article className={styles.article}>
+        <section className={styles.section}>
+          <span>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
+            commodi consequatur, nostrum sequi totam reprehenderit obcaecati
+            deserunt dolore dolores voluptates sapiente dignissimos aut adipisci
+            unde quia doloribus nulla optio! Nam.
+          </span>
+          <br />
+          <br />
+          <span>
+            <b>title</b>
+          </span>
+          <br />
+          <br />
+          <span>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
+            commodi consequatur, nostrum sequi totam reprehenderit obcaecati
+            deserunt dolore dolores voluptates sapiente dignissimos aut adipisci
+            unde quia doloribus nulla optio! Nam. Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Nostrum reprehenderit facere enim
+            quisquam explicabo harum voluptas, corrupti neque quia. Molestiae
+            eligendi corporis pariatur ducimus, velit culpa deserunt ut id! Ut!
+          </span>
+        </section>
+      </article>
+    </main>
+  );
+};
 
 export default Research;
